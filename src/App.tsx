045891fb0f31d47f2c62c9db52a8f4bdfba20934
magnayn <nigel.magnay@nirima.com>
2023-08-22 18:03:58 +0100

    Messing
    Signed-off-by: magnayn <nigel.magnay@nirima.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,54 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { gql, useQuery, useSubscription } from '@apollo/client';
+
+
+const GET_MESSAGES = gql`
+  query {
+    items {
+      name
+    }
+  }
+`;
+
+const MESSAGE_CREATED = gql`
+  subscription {
+    events {
+      topic
+      ... on ItemEvent {
+        item {
+          name
+        }
+      ... on ItemCommandEvent {
+        command
+      }
+      }
+    }
+  }
+`;
+
 
 function App() {
+
+  const dd = useQuery(GET_MESSAGES);
+  
+  const { data, loading } = useSubscription(MESSAGE_CREATED, {
+    onData: ({ data }) => {
+      console.log(data.data)
+    }
+  })
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+          <div>{dd?.data?.items?.map( 
+            (it:any) => <div>{it.name}</div>
+          )}</div>
+          <h1>upd</h1>
+          <div>
+            {JSON.stringify(data)}
+          </div>
       </header>
     </div>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,62 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+import { ApolloClient, ApolloLink, ApolloProvider, HttpLink, InMemoryCache, split } from '@apollo/client';
+import { WebSocketLink } from "@apollo/client/link/ws";
+import { SubscriptionClient } from "subscriptions-transport-ws";
+import { getMainDefinition } from '@apollo/client/utilities';
+import { GraphQLWsLink } from "@apollo/client/link/subscriptions";
+import { createClient } from "graphql-ws";
+
+const httpLink = new HttpLink({
+  uri: 'http://localhost:8080/graphql',
+});
+
+/*const wsLink = new WebSocketLink({
+  uri: `ws://localhost:8080/graphql`,
+  options: {
+    reconnect: true,
+  },
+});
+*/
+
+const wsLink = new GraphQLWsLink(
+  createClient({
+    url: "ws://localhost:8080/graphql",
+  }),
+);
+
+const terminatingLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === 'OperationDefinition' && definition.operation === 'subscription'
+    );
+  },
+  wsLink,
+  httpLink,
+);
+
+const link = ApolloLink.from([terminatingLink]);
+
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+  link,
+  cache,
+});
+
+
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <ApolloProvider client={client}>
     <App />
+  </ApolloProvider>,
   </React.StrictMode>
 );
 
